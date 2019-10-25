4f08f2262fed157a55e8bb78dd1765cdcfff7611
qing827211255 <827211255@qq.com>
2019-10-25 22:00:37 +0800

    项目初始化完毕

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+// 使用element-ui
+Vue.use(ElementUI)
 
 new Vue({
   render: h => h(App)
