60f28f99ba8a42fb235274a55d236ddd467d696d
qing827211255 <827211255@qq.com>
2019-10-25 22:28:59 +0800

    饿了么组件库引入以及vue-router初始化引入

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 // 引入element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import router from '@/router'
+// 是上面那种写法的完整版 import router from './router/index.js'
 
 Vue.config.productionTip = false
 // 使用element-ui
 Vue.use(ElementUI)
 
 new Vue({
+  router, // 挂载
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+// 引入
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+// 使用
+Vue.use(VueRouter)
+
+// 实例化
+var router = new VueRouter({
+  routes: []
+})
+
+// 导出
+export default router
